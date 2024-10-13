34f6f163dcaccd2a91f0ebdc58df1b66b739890a
viotsoft <viotsoft@gmail.com>
2024-10-13 12:57:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
-print("Hello world")
-print("my world")
-print("my dat science")
-print("my dat science master")
+class Cat():
+    def __init__(self, name):
+        self.name = name
+    def say(self):
+        print(f"{self.name} tell mau")
+
+my_cat = Cat('Murchick')
+print(f"{my_cat.name} cute cat")
+my_cat.say()
+
+class HouseCat(Cat):
+    def __init__(self, name):
+        super().__init__(name)
+    
+    def play(self):
+        print(f"{self.name} play")
+
+my_house_cat = HouseCat('Murch')
+print(f"{my_cat.name} home cat")
+my_house_cat.say()
+my_house_cat.play()
+
